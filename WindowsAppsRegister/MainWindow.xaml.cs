9ed101c5e16f41714262469638f2e1cd196fdac6
zhangpeng <scbeta@qq.com>
2019-08-08 11:58:43 +0800

    First commit.

---
+++ WindowsAppsRegister/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using WindowsAppsRegister.Model;
+
+namespace WindowsAppsRegister
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<AppxPackage> AppxPackageCollection { get; set; } = new ObservableCollection<AppxPackage>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        private void RunPowerShellScript(string script, Action<Collection<PSObject>> handler)
+        {
+            using (Runspace runspace = RunspaceFactory.CreateRunspace())
+            {
+                runspace.Open();
+                Pipeline pipeline = runspace.CreatePipeline();
+                pipeline.Commands.AddScript(script);
+                Collection<PSObject> results = pipeline.Invoke();
+                handler(results);
+                runspace.Close();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AppxPackageCollection.Clear();
+            RunPowerShellScript("Get-AppxPackage", items =>
+             {
+                 foreach(var item in items)
+                 {
+                     var package = AppxPackage.GetItem(item);
+                     if (package.IsFramework || package.NonRemovable)
+                         continue;
+
+                     RunPowerShellScript($"Get-AppxPackageManifest -Package \"{package.PackageFullName}\"", manifest =>
+                      {
+                          var document = (XmlDocument)manifest[0].BaseObject;
+                      });
+                     AppxPackageCollection.Add(package);
+                 }
+             });            
+        }
+    }
+}
---
+++ WindowsAppsRegister/Model/AppxPackage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsAppsRegister.Model
+{
+    public class AppxPackage
+    {
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public object Architecture { get; set; }
+        public string ResourceId { get; set; }
+        public string Version { get; set; }
+        public string PackageFullName { get; set; }
+        public string InstallLocation { get; set; }
+        public bool IsFramework { get; set; }
+        public string PackageFamilyName { get; set; }
+        public string PublisherId { get; set; }
+        public bool IsResourcePackage { get; set; }
+        public bool IsBundle { get; set; }
+        public bool IsDevelopmentMode { get; set; }
+        public bool NonRemovable { get; set; }
+        public bool IsPartiallyStaged { get; set; }
+        public object SignatureKind { get; set; }
+        public object Status { get; set; }
+
+        public static AppxPackage GetItem(PSObject obj)
+        {
+            var type = typeof(AppxPackage);
+            var model = new AppxPackage();
+            foreach( var pi in type.GetProperties())
+            {
+                pi.SetValue(model, obj.Properties[pi.Name].Value);
+            }
+            return model;
+        }
+    }
+}
